2181342910a784859eb97649e6fb4bffdf80447a
dasaribhumika <bhumikadasari0@gmail.com>
2024-10-21 08:55:54 -0400

    Added pysqlite3 imports

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 import streamlit as st
+__import__ ('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 from langchain_community.document_loaders import WebBaseLoader
 
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
+
+
 
 
 def create_streamlit_app(llm, portfolio, clean_text):
